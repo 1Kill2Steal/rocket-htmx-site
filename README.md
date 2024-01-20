79e5c175e1dd517c4bffca16ab0686dda5dad0c3
1Kill2Steal <imliterallyhim@duck.com>
2024-01-20 20:25:21 +0200

    20/01/2023 - UI changes + small functionality

--- README.md
+++ README.md
@@ -7,5 +7,10 @@ I could try using Trunk to make it a web application for the future (not guarant
 1. [Rust lang w/ cargo](https://www.rust-lang.org/learn/get-started)
 
 ## To use the application
-1. Run `cargo build` (You only need to run it once)
+1. Run `cargo install` (You only need to run it once)
 2. Run `cargo run` OR `cargo run --release`
+
+## TODO:
+- SQLite integration (when? idk...)
+- A lot of UI Improvement
+- Buying a real domain and hosting on it- (probably not soon)
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 // /src/main.rs
-#[macro_use]
-extern crate rocket;
 
-/*
-#[get("/")]
-fn index() -> &'static str {
-    "Rust + HTMX test website!"
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+use rocket::response::content::RawHtml;
+
+use rusqlite::{Connection, Result};
+
+#[get("/nav_content")]
+fn nav_content() -> rocket::response::content::RawHtml<&'static str> {
+    rocket::response::content::RawHtml(r#"
+        <nav>
+            <div class="nav_item">
+                <a href="./index.html">Homepage</a>
+            </div>
+            <div class="nav_item">
+                <a href="./sign_up.html">Sign Up</a>
+            </div>
+            <div class="nav_item">
+                <a href="./login.html">Log In</a>
+            </div>
+        </nav>
+    "#)
 }
-*/
 
 #[get("/update")]
 fn update() -> &'static str {
@@ -19,5 +34,6 @@ fn rocket() -> _ {
     use rocket::fs::FileServer;
     rocket::build()
         .mount("/", FileServer::from("static"))
-        .mount("/", routes![/*index, */update])
+        .mount("/", routes![nav_content])
+        .mount("/", routes![update])
 }
